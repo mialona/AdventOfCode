217ee0b49680cc2c002bf3074dd1dd3a5958260e
mialona <mialona@unirioja.es>
2025-01-14 22:21:47 +0100

    Test Day21P2

--- 2024/Day21/Part2.py
+++ 2024/Day21/Part2.py
@@ -1,16 +1,38 @@
 def paths(i, j, path):
-    result = [];
     if i > 0:
-        result += paths(i - 1, j, path + ['v']);
-    if i < 0:
-        result += paths(i + 1, j, path + ['^']);
+        laux1 = ['v' for a in range(i)];
+    elif i < 0:
+        laux1 = ['^' for a in range(-i)];
+    else:
+        laux1 = [];
     if j > 0:
-        result += paths(i, j - 1, path + ['>']);
-    if j < 0:
-        result += paths(i, j + 1, path + ['<']);
-    if i == 0 and j == 0:
-        return [path];
-    return result;
+        laux2 = ['>' for b in range(j)];
+    elif j < 0:
+        laux2 = ['<' for b in range(-j)];
+    else:
+        laux2 = [];
+##    if i == 0:
+##        return [path + laux2];
+##    elif j == 0:
+##        return [path + laux1];
+##    else:
+##        return [path + laux1 + laux2, path + laux2 + laux1];
+    if i < 0:
+        if j < 0:
+            return [path + laux1 + laux2, path + laux2 + laux1];
+        elif j > 0:
+            return [path + laux2 + laux1];
+        else:
+            return [path + laux1];
+    elif i > 0:
+        if j < 0:
+            return [path + laux1 + laux2];
+        elif j > 0:
+            return [path + laux1 + laux2, path + laux2 + laux1];
+        else:
+            return [path + laux1];
+    else:
+        return [path + laux2];
 
 numpad = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '#', '0', 'A'];
 def inputsnumpad(buttons):
@@ -74,20 +96,34 @@ with open('input.gnumeric', 'r') as file:
     sum = 0;
     for i in range(len(codes)):
         smin = 10**10;
-        poss = inputsnumpad(codes[i]);
-        lposs = len(min(poss, key = len));
-        poss = [e for e in poss if len(e) == lposs];
-
-        poss2 = [];
-        for e in poss:
-            poss2 += inputsdirpad(e);
-        lposs2 = len(min(poss2, key = len));
-        poss2 = [e for e in poss2 if len(e) == lposs2];
-
-        poss3 = [];
-        for e in poss2:
-            poss3 += inputsdirpad(e);
-        lposs3 = len(min(poss3, key = len));
-
-        sum += lposs3*numpart[i];
+        result = inputsnumpad(codes[i]);
+        for j in range(2):
+            result2 = [];
+            for k in result:
+                result2 += inputsdirpad(k);
+            result = result2;
+        for l in result:
+            smin = min(smin, len(l)*numpart[i]);
+        sum += smin;
     print(sum);
+
+##    sum = 0;
+##    for i in range(len(codes)):
+##        smin = 10**10;
+##        poss = inputsnumpad(codes[i]);
+##        lposs = len(min(poss, key = len));
+##        poss = [e for e in poss if len(e) == lposs];
+##
+##        poss2 = [];
+##        for e in poss:
+##            poss2 += inputsdirpad(e);
+##        lposs2 = len(min(poss2, key = len));
+##        poss2 = [e for e in poss2 if len(e) == lposs2];
+##
+##        poss3 = [];
+##        for e in poss2:
+##            poss3 += inputsdirpad(e);
+##        lposs3 = len(min(poss3, key = len));
+##
+##        sum += lposs3*numpart[i];
+##    print(sum);
